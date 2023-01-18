1ee3138f9efb23f2c0c84acdb4fae8cf7333a16b
Ahzam Akhtar <ahzam8186@gmail.com>
2023-01-19 00:37:11 +0530

    header updated

--- components/InsideComponent/Header.js
+++ components/InsideComponent/Header.js
@@ -6,8 +6,12 @@ const Header = () => {
     <>
       <header class="text-gray-600 body-font">
         <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <button onClick={()=> router.push("/main")} class="bg-white text-black py-2 px-4 rounded-3xl inline-flex items-center">
+        <button onClick={()=> router.push("/upload")} class="bg-white text-black py-2 px-4 rounded-3xl inline-flex items-center mx-5">
             EXPLORE
+          </button>
+
+          <button onClick={()=> router.push("/main")} class="bg-white text-black py-2 px-4 rounded-3xl inline-flex items-center">
+            People
           </button>
 
           <button onClick={()=> router.push("/myspace")} class="bg-white text-black py-2 px-4 rounded-3xl inline-flex items-center mx-5">
